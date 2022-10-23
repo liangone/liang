9f7a1767f8750726a94e850674bf0fb62bb32a3a
dongliang <2960699981@qq.com>
2022-10-23 19:01:52 +0800

    博客初始化

---
+++ .temp/internal/app-enhancers.js
@@ -0,0 +1,19 @@
+import m0 from "C:\\Users\\86191\\Desktop\\个人博客\\liang\\.temp\\app-enhancers\\0.js"
+import m1 from "C:\\Users\\86191\\Desktop\\个人博客\\liang\\.temp\\app-enhancers\\data-block.js"
+import m2 from "C:\\Users\\86191\\Desktop\\个人博客\\liang\\.temp\\app-enhancers\\global-components-1.js"
+import m3 from "C:\\Users\\86191\\Desktop\\个人博客\\liang\\.temp\\app-enhancers\\1.js"
+import m4 from "C:\\Users\\86191\\Desktop\\个人博客\\liang\\.temp\\app-enhancers\\2.js"
+import m5 from "C:\\Users\\86191\\Desktop\\个人博客\\liang\\.temp\\app-enhancers\\3.js"
+import m6 from "C:\\Users\\86191\\Desktop\\个人博客\\liang\\.temp\\app-enhancers\\4.js"
+import m7 from "C:\\Users\\86191\\Desktop\\个人博客\\liang\\.temp\\app-enhancers\\auto-sidebar-enhance.js"
+
+export default [
+  m0,
+  m1,
+  m2,
+  m3,
+  m4,
+  m5,
+  m6,
+  m7
+]
--- docs/README.md
+++ docs/README.md
@@ -1,10 +1,10 @@
 ---
 home: true
 heroImage: /assets/img/hero.png
-heroText: Hero 标题
-tagline: Hero 副标题
-actionText: 快速上手 →
-actionLink: /zh/guide/
+heroText: 冠希的个人博客
+# tagline: Hero 副标题
+actionText: 开始学习 →
+actionLink: /interview/CSS/01
 features:
 - title: 简洁至上
   details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
