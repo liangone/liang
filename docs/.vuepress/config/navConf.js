dc9652c60267a50eb3ca979d1240d602a01619ec
dongliang <2960699981@qq.com>
2022-10-24 14:58:14 +0800

    面试

--- docs/.vuepress/config/navConf.js
+++ docs/.vuepress/config/navConf.js
@@ -2,28 +2,45 @@ module.exports = [
 	{ text: '首页', link: '/' },
 	{ text: '前端', link: '/web/' },
 	{
-		text: '面试', 
-		link: '/interview/', 
+		text: '面试',
+		link: '/interview/',
 		items: [
 			{
 				text: '基础面试题', items: [
 					{ text: 'CSS', link: '/interview/CSS/01' },
 					{ text: 'ES6', link: '/interview/ES6/01' },
-					{ text: 'git', link: '/interview/git/' },
-					{ text: 'js', link: '/interview/js/' },
-					{ text: 'linux', link: '/interview/linux/' },
-					{ text: 'node', link: '/interview/node/' },
-					{ text: 'react', link: '/interview/react/' },
-					{ text: 'typescript', link: '/interview/typescript/' },
-					{ text: 'vue', link: '/interview/vue/' },
-					{ text: '打包', link: '/interview/打包/' },
-					{ text: '设计模式', link: '/interview/设计模式/' },
-					{ text: '网络协议', link: '/interview/网络协议/' },
+					{ text: 'Git', link: '/interview/git/01' },
+					{ text: 'JS', link: '/interview/js/01' },
+					{ text: 'Linux', link: '/interview/linux/01' },
+					{ text: 'Node', link: '/interview/node/01' },
+					{ text: 'React', link: '/interview/react/01' },
+					{ text: 'TypeScript', link: '/interview/typescript/01' },
+					{ text: 'Vue', link: '/interview/vue/01' },
+					{ text: '打包', link: '/interview/打包/01' },
+					{ text: '设计模式', link: '/interview/设计模式/01' },
+					{ text: '网络协议', link: '/interview/网络协议/01' },
 				]
 			},
+			{
+				text: '进阶面试题', items: [
+					{ text: 'html', link: '/interview/interviewMove/01' },
+					{ text: 'css', link: '/interview/interviewMove/02' },
+					{ text: 'javascript上', link: '/interview/interviewMove/03' },
+					{ text: 'javascript下', link: '/interview/interviewMove/04' },
+					{ text: 'vue上', link: '/interview/interviewMove/05' },
+					{ text: 'vue下', link: '/interview/interviewMove/06' },
+					{ text: 'react上', link: '/interview/interviewMove/07' },
+					{ text: 'react下', link: '/interview/interviewMove/08' },
+					{ text: '计算机网络', link: '/interview/interviewMove/09' },
+					{ text: '浏览器', link: '/interview/interviewMove/10' },
+					{ text: '优化', link: '/interview/interviewMove/11' },
+					{ text: '手写代码', link: '/interview/interviewMove/12' },
+					{ text: '输出结果', link: '/interview/interviewMove/13' },
+					{ text: 'typescript', link: '/interview/interviewMove/14' },
+				]
+			}
 		]
 	},
-	{ text: '算法', link: '/arithmetic/' },
-	{ text: '数据结构', link: '/dataStructure/' },
+	{ text: '算法与数据结构', link: '/算法与数据结构/01' },
 	{ text: 'GitHub', link: 'https://github.com/294169089l/liang' },
 ];
